80fa4808c3443bff538e0c1ec92e99db307eed29
herdanuyp <heh.danu@gmail.com>
2020-03-10 17:02:06 +0800

    Changed

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,21 +1,19 @@
-import Link from 'next/link';
-import styled from 'styled-components';
-import FocusLock from 'react-focus-lock';
+import Link from 'next/link'
+import styled from 'styled-components'
 
-import Burger from './Burger';
-import Menu from './Menu';
-import { useOnClickOutside } from './hooksEffect';
+import Burger from './Burger'
+import Menu from './Menu'
 
 const link = [
   { link: '/about', label: 'about us' },
   { link: '/product', label: 'product' },
   { link: '/contact', label: 'contact' }
-];
+]
 
 const NavigationWrapper = styled.div`
   width: 100%;
   min-height: 84px;
-`;
+`
 
 const NavLinks = styled.ul`
   list-style-type: none;
@@ -47,44 +45,39 @@ const NavLinks = styled.ul`
     transition: 0.5s;
     text-transform: uppercase;
   }
-`;
+`
 
-const BurgerWrapper = styled.div``;
+const BurgerWrapper = styled.div``
 
 function Navigation() {
-  const [open, setOpen] = React.useState(false);
-  const node = React.useRef();
-  const menuId = 'main-menu';
-  const [isSmallScreen, setIsSmallScreen] = React.useState(false);
+  const [open, setOpen] = React.useState(false)
+  const menuId = 'main-menu'
+  const [isSmallScreen, setIsSmallScreen] = React.useState(false)
 
   React.useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 768px)');
-    mediaQuery.addListener(handleMediaQueryChange);
-    handleMediaQueryChange(mediaQuery);
+    const mediaQuery = window.matchMedia('(max-width: 768px)')
+    mediaQuery.addListener(handleMediaQueryChange)
+    handleMediaQueryChange(mediaQuery)
 
     return () => {
-      mediaQuery.removeListener(handleMediaQueryChange);
-    };
-  }, []);
+      mediaQuery.removeListener(handleMediaQueryChange)
+    }
+  }, [])
 
   const handleMediaQueryChange = (mediaQuery) => {
     if (mediaQuery.matches) {
-      setIsSmallScreen(true);
+      setIsSmallScreen(true)
     } else {
-      setIsSmallScreen(false);
+      setIsSmallScreen(false)
     }
-  };
-
-  useOnClickOutside(node, () => setOpen(false));
+  }
 
   return (
     <NavigationWrapper>
       {isSmallScreen ? (
-        <BurgerWrapper ref={node}>
-          <FocusLock disabled={!open}>
-            <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
-            <Menu open={open} setOpen={setOpen} id={menuId} />
-          </FocusLock>
+        <BurgerWrapper>
+          <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
+          <Menu open={open} setOpen={setOpen} id={menuId} />
         </BurgerWrapper>
       ) : (
         <NavLinks>
@@ -98,7 +91,7 @@ function Navigation() {
         </NavLinks>
       )}
     </NavigationWrapper>
-  );
+  )
 }
 
-export default Navigation;
+export default Navigation
